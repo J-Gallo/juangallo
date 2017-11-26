756a938ffeb4d5fa4b5c87c2b0c44a82337ad82b
Juan Gallo <juani.reicko@gmail.com>
2017-11-26 15:54:37 -0300

    Compression

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
 
+const compression = require('compression');
 const port = parseInt(process.env.PORT, 10) || 3001
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -10,11 +11,11 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 
-
 app.prepare() 
 .then(() => {
   const server = express();
 
+  server.use(compression());
   server.use(bodyParser.json());
   server.use(bodyParser.urlencoded({ extended: false }));
   server.use(cookieParser());
