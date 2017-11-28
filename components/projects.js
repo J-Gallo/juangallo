690de2f1cc129e893dd43757053320977a070e82
Juan Gallo <juani.reicko@gmail.com>
2017-11-28 14:22:11 -0300

    Links

--- components/projects.js
+++ components/projects.js
@@ -17,28 +17,36 @@ class WebProjects extends React.Component {
             <div className="project-data">
               <h3>Argentum Online Web </h3>
             </div>
-          <img src="/static/aoweb.jpg" alt="Imagen de Proyecto Argentum Online Web" />
+          <a href="http://www.argentumonlineweb.com" target="_blank">            
+            <img src="/static/aoweb.jpg" alt="Imagen de Proyecto Argentum Online Web" />
+          </a>
         </div>
         <div className="project-container">
           <div className="project-overlay tanIntensa"></div>
           <div className="project-data">
             <h3>Tan Intensa </h3>
           </div>
-          <img src="/static/tanintensa.jpg" alt="Imagen de Proyecto Tan Intensa" />        
+          <a href="https://www.tanintensa.com" target="_blank">
+          <img src="/static/tanintensa.jpg" alt="Imagen de Proyecto Tan Intensa" />  
+          </a>      
         </div>
         <div className="project-container">
           <div className="project-overlay lutionAds"></div>
           <div className="project-data">
             <h3>Lution Ads</h3>
           </div>
-          <img src="/static/LutionAds.jpg" alt="Imagen de Proyecto Lution Ads" />     
+          <a href="https://www.lutionads.com" target="_blank">          
+            <img src="/static/LutionAds.jpg" alt="Imagen de Proyecto Lution Ads" />     
+          </a>
         </div>
         <div className="project-container">
           <div className="project-overlay tanIntensa"></div>
           <div className="project-data">
             <h3>Twitter List</h3>
           </div>
-          <img src="/static/twitterlist.jpg" alt="Imagen de Proyecto Twitter List" />     
+          <a href="https://twitter-list.now.sh" target="_blank">          
+            <img src="/static/twitterlist.jpg" alt="Imagen de Proyecto Twitter List" />
+          </a>
         </div>
         <style jsx>{`
           .projects-container {
